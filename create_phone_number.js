0154da16bcb6ca5337c7e8a51c70bfec65f29ae3
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-02-21 19:53:50 +0100

    Create create_phone_number.js

---
+++ create_phone_number.js
@@ -0,0 +1,14 @@
+function createPhoneNumber(numbers){
+  let number = "(";
+  for (let i = 0; i < numbers.length; i++) {
+    number += numbers[i];
+    if (i == 2) {
+      number += ") ";
+    }
+    if (i == 5) {
+      number += "-";
+    }
+  }
+  
+  return number;
+}
