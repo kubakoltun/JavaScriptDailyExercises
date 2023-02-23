ec1c0c9b46d632d4f77fd92cb8782030bc419158
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-02-23 18:43:04 +0100

    Create is_valid_walk.js

---
+++ is_valid_walk.js
@@ -0,0 +1,32 @@
+function is_valid_walk(walk) {
+  inTime = false;
+  walkingCord = 0;
+  if (walk.length != 10) {
+    inTime = false;
+  } 
+  else {
+    for (var i = 0; i < walk.length; i++) {
+      if (walk[i] == 'n') {
+        walkingCord += 1;
+      }
+      else if (walk[i] == 's') {
+        walkingCord -= 1;
+      }
+      else if (walk[i] == 'w') {
+        walkingCord += 100;
+      }
+      else if (walk[i] == 'e') {
+        walkingCord -= 100;
+      }
+    }
+    
+    if (walkingCord == 0) {
+      inTime = true;
+    } 
+    else {
+      inTime = false;
+    }
+  }
+  
+  return inTime; 
+}
