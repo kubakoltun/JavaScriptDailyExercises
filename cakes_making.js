dfb98580aef64ce9927dfc875a2430a23e8a171b
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-02-24 16:55:16 +0100

    Create cakes_making.js

---
+++ cakes_making.js
@@ -0,0 +1,14 @@
+function cakes_making (recipe, available) {
+  let cakes = Infinity;
+  for (var i in recipe) {
+    if (!available[i] || available[i] < recipe[i]) {
+      return 0;
+    }
+    const possibleCakes = Math.floor(available[i]/recipe[i]);
+    if (possibleCakes < cakes) {
+      cakes = possibleCakes;
+    }
+  }
+  
+  return cakes;
+}
