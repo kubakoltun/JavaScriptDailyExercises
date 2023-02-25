a48d6a68c738e35ea971cf72f1ab33314dbc0862
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-02-25 12:19:41 +0100

    Create rot13_encoding_decoding.js

---
+++ rot13_encoding_decoding.js
@@ -0,0 +1,24 @@
+function rot13(str) {
+  
+    let newStr = "";
+    let charCode = 0;
+  
+    for (let i = 0; i < str.length; i++) {
+      if (str.charAt(i).match(/[a-zA-Z]/g)) {
+         charCode = (str[i].charCodeAt());
+          if (str[i].toLowerCase().charCodeAt() > 109) {
+            charCode -= 13;
+            newStr += String.fromCharCode(charCode);
+          }
+          else {
+            charCode += 13;
+            newStr += String.fromCharCode(charCode);
+          }
+      }
+      else {
+        newStr += str.charAt(i);
+      }
+    }
+    
+    return newStr;
+  }
