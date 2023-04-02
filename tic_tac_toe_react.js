fc5afa0bf9959f50b48f0810237df773ef453fd7
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-04-02 06:49:09 +0200

    Update tic_tac_toe_react.js

--- tic_tac_toe_react.js
+++ tic_tac_toe_react.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+//ongoin updates
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
