23f03997fbe3a445f3d8095cfb14f235d12a1592
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-04-20 21:10:36 +0200

    Create lines_gradient.js

---
+++ lines_gradient.js
@@ -0,0 +1,56 @@
+function setup() {
+  createCanvas(512, 512);
+  background(255);
+}
+
+var x0 = -1;
+var y0 = -1;
+var x1 = -1;
+var y1 = -1;
+
+function mousePressed() {
+  x0 = mouseX;
+  y0 = mouseY;
+}
+
+function mouseDragged() {
+  x1 = mouseX;
+  y1 = mouseY;
+  background(255);
+  noStroke();
+  fill('red');
+  ellipse(x0 - 3, y0 - 3, 6);
+  fill('green');
+  ellipse(x1 - 3, y1 - 3, 6);
+}
+
+function mouseReleased() {
+  background(255);
+  loadPixels();
+  draw_line();
+  updatePixels();
+}
+
+function set_pixel(x, y, c) {
+  idx = (y * 512 + x) * 8; //ew 4
+  pixels[idx] = -c;
+  pixels[idx + 1] = c;
+  pixels[idx + 2] = 0;
+  pixels[idx + 3] = 255;
+}
+
+function draw_line() {
+  let dx = x1 - x0;
+  let dy = y1 - y0;
+  let a = dy / dx;
+  let b = y0 - a * x0;
+
+  for(y=0; y<height; y++) {
+    for(x=0; x<width; x++) {
+      let dxy = (dy/dx)*(x-x0)-(y-y0);
+      let dxyd = dxy*2*dx;
+      // dxy
+      set_pixel(x, y, Math.round(dxyd));
+    }
+  }
+}
