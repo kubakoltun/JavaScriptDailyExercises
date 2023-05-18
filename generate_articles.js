cb32b09dd3d03f35d47f61b676e73e18d2b438be
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-05-18 12:48:01 +0200

    Create generate_articles.js

---
+++ generate_articles.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const stream = fs.createWriteStream('Towary.txt');
+
+for (let i = 1; i <= 10000; i++) {
+    let weight = Math.floor((Math.random() * 100) + 10);
+    let articles = `${i} ${waga}\n`;
+
+    stream.write(articles, 'utf-8', (err) => {
+        if (err) {
+            console.error(`Writing error ${err}`);
+        }
+    });
+}
+
+stream.end();
