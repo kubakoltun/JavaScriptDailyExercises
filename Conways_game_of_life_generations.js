355d275b8e7ada41dc05e855f4792d71d57e0fe4
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-05-20 15:02:57 +0200

    Update Conways_game_of_life_generations.js

--- Conways_game_of_life_generations.js
+++ Conways_game_of_life_generations.js
@@ -1,13 +1,15 @@
 function getGeneration(cells, generations) {
+  if (cells.length === 0 || cells[0].length === 0) {
+    return [[]]; 
+  }
+
   let numRows = cells.length;
   let numCols = cells[0].length;
-  let evolution = cells;
-  //efficiency is completly off - need to add more function_wise approach
-  //generations do not scale - meaning that array does not change while it should in case of a new alive cells
+  let evolution = cells.slice(); 
+
   for (let g = 0; g < generations; g++) {
     let newGeneration = Array(numRows).fill().map(() => Array(numCols).fill(0));
-    
-    //reduce the usage of checking neighbours to a possibility of their occurrence
+
     for (let i = 0; i < numRows; i++) {
       for (let j = 0; j < numCols; j++) {
         let neighbours = 0;
@@ -35,47 +37,49 @@ function getGeneration(cells, generations) {
         }
       }
     }
+
     evolution = newGeneration;
   }
 
-  // check if the first row is all 0s
-  while (evolution[0].every(cell => cell === 0)) {
-    evolution.shift(); // remove the first row
-    evolution.push(Array(numCols).fill(0)); // add a new row at the bottom with all 0s
-  }
+  // Crop the evolution array
+  let firstRowIndex = -1;
+  let lastRowIndex = -1;
+  let firstColIndex = -1;
+  let lastColIndex = -1;
 
-  // check if the last row has any live cells
-  if (!evolution[numRows - 1].some(cell => cell === 1)) {
-    evolution.pop(); // remove the last row
-  }
-  let lastRow = evolution[numRows - 1];
-  let newLastRow = Array(numCols).fill(0);
-  
-  for (let j = 0; j < numCols; j++) {
-    let neighbours = 0;
-    for (let di = -1; di <= 1; di++) {
-      for (let dj = -1; dj <= 1; dj++) {
-        if (di === 0 && dj === 0) continue;
-        let ni = numRows - 2 + di;
-        let nj = j + dj;
-        if (ni < 0 || ni >= numRows || nj < 0 || nj >= numCols) continue;
-        if (evolution[ni][nj] === 1) neighbours++;
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      if (evolution[i][j] === 1) {
+        if (firstRowIndex === -1 || i < firstRowIndex) {
+          firstRowIndex = i;
+        }
+        if (lastRowIndex === -1 || i > lastRowIndex) {
+          lastRowIndex = i;
+        }
+        if (firstColIndex === -1 || j < firstColIndex) {
+          firstColIndex = j;
+        }
+        if (lastColIndex === -1 || j > lastColIndex) {
+          lastColIndex = j;
+        }
       }
-   }
+    }
+  }
 
-   if (evolution[numRows - 1][j] === 1) {
-     if (neighbours < 2 || neighbours > 3) {
-       newLastRow[j] = 0;
-     } else {
-       newLastRow[j] = 1;
-     }
-   } else {
-     if (neighbours === 3) {
-       newLastRow[j] = 1;
-     }
-   }
- }
- evolution.push(newLastRow);
+  if (firstRowIndex === -1 || lastRowIndex === -1 || firstColIndex === -1 || lastColIndex === -1) {
+    return [[]]; 
+  }
 
- return evolution;
+  if (lastRowIndex < numRows - 1) {
+    lastRowIndex++;
+  }
+  if (lastColIndex < numCols - 1) {
+    lastColIndex++;
+  }
+
+  let croppedEvolution = evolution
+    .slice(firstRowIndex, lastRowIndex + 1)
+    .map(row => row.slice(firstColIndex, lastColIndex + 1));
+
+  return croppedEvolution;
 }
