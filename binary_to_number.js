e5aa06a5ab2264630d9fc8b3048d4d6f9b48a9c1
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-06-12 18:13:21 +0200

    Update binary_to_number.js

--- binary_to_number.js
+++ binary_to_number.js
@@ -2,8 +2,10 @@ const binary_to_number = arr => {
   var hold = 0;
   var out = 0;
   var j = 0;
+  
   for (var i = arr.length-1; i >= 0; i--) {
     out = 0;
+    
     if (arr[i] == 1) {
       out = Math.pow(2, j);
     }
