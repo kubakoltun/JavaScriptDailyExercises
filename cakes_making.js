c89df11ca63ae57bcd9e7abcbd0bd0ba777ae3f7
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-06-12 18:13:39 +0200

    Update cakes_making.js

--- cakes_making.js
+++ cakes_making.js
@@ -1,9 +1,11 @@
 function cakes_making (recipe, available) {
   let cakes = Infinity;
+  
   for (var i in recipe) {
     if (!available[i] || available[i] < recipe[i]) {
       return 0;
     }
+    
     const possibleCakes = Math.floor(available[i]/recipe[i]);
     if (possibleCakes < cakes) {
       cakes = possibleCakes;
