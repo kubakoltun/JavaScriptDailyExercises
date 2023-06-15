e757e646e082717d624fdc23ecf5abff673831af
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-06-15 09:24:01 +0200

    Update all_up_case.js

--- all_up_case.js
+++ all_up_case.js
@@ -1,5 +1,6 @@
 String.prototype.all_up_case = function () {
   var splitIn = this.split(' ');
+  
   for (var i = 0; i < splitIn.length; i++) {
     splitIn[i] = splitIn[i].charAt(0).toUpperCase() + splitIn[i].substring(1);  
   }
