51aae0b23c48d0bb6a535d88e9d81fefd8e57e3a
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-09-14 21:08:53 +0200

    Update count_bananas.js

--- count_bananas.js
+++ count_bananas.js
@@ -1,25 +1,18 @@
 function count_bananas(total) {
-   if (total <= 0) {
-    return false;
+   if (total < 67) {
+    return "false";
   }
 
-  let bananas = 0;
-  let apples = 0;
-  let oranges = 0;
-  let pears = 0;
-  let lemons = 0;
-  let tomatoes = 0;
+  const apples = Math.round(total / (3 + 1 / 6 + 1 + 40 / 100 + 17));
+  const oranges = apples + 17;
+  const pears = Math.round(oranges * (60 / 100));
+  const lemons = Math.round(pears * (5 / 6));
+  const bananas = apples * 3;
+  const tomatoes = total - (apples + oranges + pears + lemons + bananas);
 
-  oranges =apples + 17;
-  pears =  Math.round(oranges * 0.6);
-  lemons =  Math.round(pears * (5 / 6));
-  apples =  Math.round(bananas / 3);
-
-  tomatoes = total - bananas - apples - oranges - pears - lemons;
-
-  if (total < bananas + apples + oranges + pears + lemons) {
-    return false;
+  if (tomatoes > 0) {
+    return bananas;
+  } else {
+    return "false";
   }
-
-  return Math.max(bananas, total - tomatoes);
 }
