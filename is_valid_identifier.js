ba2079ac760f7e986d2fd68fcfbb913b855234eb
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-09-18 23:39:05 +0200

    Update is_valid_identifier.js

--- is_valid_identifier.js
+++ is_valid_identifier.js
@@ -1,17 +1,3 @@
 function is_valid_identifier(idn) {
-  if (!idn) {
-    return false;
-  }
-
-  if (!/^[a-zA-Z_$]/.test(idn[0])) {
-    return false;
-  }
-
-  for (let i = 1; i < idn.length; i++) {
-    if (!/^[a-zA-Z0-9_$]/.test(idn[i])) {
-      return false;
-    }
-  }
-
-  return true;
+  return (/^[a-zA-Z_$][\w$]*$/).test(idn)
 }
