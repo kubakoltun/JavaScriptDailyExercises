b7ca9eafc5140f61b4f5a471faddd58166026a59
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-09-22 18:14:37 +0200

    Create unlucky_days.js

---
+++ unlucky_days.js
@@ -0,0 +1,13 @@
+function unlucky_days(year) {
+  let count = 0;
+
+  for (let month = 0; month < 12; month++) {
+    const date = new Date(year, month, 13);
+
+    if (date.getDay() === 5) {
+      count++;
+    }
+  }
+
+  return count;
+}
