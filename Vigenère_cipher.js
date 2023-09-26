384304344a1d092841ad8f22faf4af51dbaa8136
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-09-26 18:32:30 +0200

    Update Vigenère_cipher.js

--- Vigenère_cipher.js
+++ Vigenère_cipher.js
@@ -1,36 +1,57 @@
-function VigenèreCipher(key, abc) {
+function Vigenère_cipher(key, abc) {  
   this.encode = function (str) {
     let res = "";
     let letter = "";
+    let j = 0;
+    
     for (let i = 0; i < str.length; i++) {
-      let shiftC = key.charCodeAt(i)-97;
+      let shiftC = key.charCodeAt(j)-97;
+
+      letter = String.fromCharCode(str.charCodeAt(i)+shiftC);
       
-      letter = String.fromCharCode(str.charCodeAt(i)+shiftC)
-      
-      if (str.charCodeAt(i)-97+shiftC > 25) {
-        let period = str.charCodeAt(i)-97+shiftC - 25;
-        letter = String.fromCharCode(period+97);
+      if (!abc.includes(String.fromCharCode(str.charCodeAt(i)))) {
+        res += String.fromCharCode(str.charCodeAt(i))
+      } else {
+        if (str.charCodeAt(i)-97+shiftC > 25) {
+          let period = str.charCodeAt(i)-97+shiftC - 25;
+          letter = String.fromCharCode(period+96);
+        }
+        res += letter;
       }
-      
-      res += letter;
+      j++;
+      if (j === key.length) {
+        j = 0;
+      }
     }
+  
     return res;
   };
   this.decode = function (str) {
+    console.log(str)
     let res = "";
     let letter = "";
+    let j = 0;
+
     for (let i = 0; i < str.length; i++) {
-      let shiftC = (key.charCodeAt(i)-97);
-      
-      letter = String.fromCharCode(str.charCodeAt(i)+25-shiftC)
-      
-      if (str.charCodeAt(i)-97+25-shiftC > 25) {
-        let period = str.charCodeAt(i)-97+shiftC - 25;
-        letter = String.fromCharCode(period+97);
+      let shiftC = (key.charCodeAt(j)-97);
+
+      letter = String.fromCharCode(str.charCodeAt(i)-shiftC);
+
+      if (!abc.includes(String.fromCharCode(str.charCodeAt(i)))) {
+        res += String.fromCharCode(str.charCodeAt(i))
+      } else {
+        if (str.charCodeAt(i)-97-shiftC < 0) {
+          let period = str.charCodeAt(i)-97-shiftC + 25;
+          letter = String.fromCharCode(period+98);
+        }
+        res += letter;
       }
-      
-      res += letter;
+      j++;
+      if (j === key.length) {
+        j = 0;
+      }
     }
+  
     return res;
   };
 }
