94ab67ee0a44250b9948d36ad9745470ca68c693
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-09-27 17:14:34 +0200

    Create next_smaller_number.js

---
+++ next_smaller_number.js
@@ -0,0 +1,30 @@
+function nextSmaller(n) {
+  const digits = Array.from(String(n), Number);
+  let i = digits.length - 2;
+  
+  while (i >= 0 && digits[i] <= digits[i + 1]) {
+    i--;
+  }
+
+  if (i === -1) {
+    return -1;
+  }
+
+  let j = digits.length - 1;
+  while (digits[j] >= digits[i]) {
+    j--;
+  }
+
+  [digits[i], digits[j]] = [digits[j], digits[i]];
+
+  const rightOfPivot = digits.slice(i + 1);
+  rightOfPivot.sort((a, b) => b - a);
+
+  const result = [...digits.slice(0, i + 1), ...rightOfPivot];
+  
+  if (result[0] === 0) {
+    return -1;
+  }
+
+  return parseInt(result.join(''));
+}
