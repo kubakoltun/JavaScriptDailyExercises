b34f4d8ae8426b1a8f14fb089d1af54eb888c552
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-10-11 18:40:32 +0200

    Update Conways_game_of_life_generations.js

--- Conways_game_of_life_generations.js
+++ Conways_game_of_life_generations.js
@@ -1,20 +1,24 @@
 function getGeneration(cells, generations) {
-  var ce = JSON.parse(JSON.stringify(cells));
-  var minY;
-  var maxY;
-  var minX;
-  var maxX;
+  let ce = JSON.parse(JSON.stringify(cells));
+  let minY;
+  let maxY;
+  let minX;
+  let maxX;
 
   for (var i = 1; i <= generations; i++) {
     expandUniverse();
-    ce = ce.map(function(r, row){return r.map(function(c, col){return setValue(row, col, -c);});});
+    ce = ce.map(function(r, row) {
+          return r.map(function(c, col) {
+            return setValue(row, col, -c);
+          });
+    });
     getBoundaries();
     shrinkUniverse();
   }
 
   function setValue(row, col, sum) {
-    for (var r = row - 1; r <= row + 1; r++)
-      for (var c = col - 1; c <= col + 1; c++)
+    for (let r = row - 1; r <= row + 1; r++)
+      for (let c = col - 1; c <= col + 1; c++)
         sum += (ce[r] && ce[r][c]) | 0;
 
     return +(sum == 3 || sum == 2 && ce[row][col]);
@@ -23,27 +27,35 @@ function getGeneration(cells, generations) {
   function getBoundaries() {
     minY = maxY = minX = maxX = -1;
     
-    ce.forEach(function(r, row){r.forEach(function(c, col){
-      if (c == 1) {
-        minY = minY == -1 ? row : Math.min(minY, row);
-        maxY = maxY == -1 ? row : Math.max(maxY, row);
-        minX = minX == -1 ? col : Math.min(minX, col);
-        maxX = maxX == -1 ? col : Math.max(maxX, col);
-      }
-    });});
+    ce.forEach(function(r, row) {
+      r.forEach(function(c, col) {
+        if (c == 1) {
+          minY = minY == -1 ? row : Math.min(minY, row);
+          maxY = maxY == -1 ? row : Math.max(maxY, row);
+          minX = minX == -1 ? col : Math.min(minX, col);
+          maxX = maxX == -1 ? col : Math.max(maxX, col);
+        }
+      });
+    });
   }
 
   function expandUniverse() {
     var a = [], b = [];
 
-    ce[0].forEach(function(){a.push(0); b.push(0);});
+    ce[0].forEach(function() {
+      a.push(0); b.push(0);
+    });
     ce.unshift(a);
     ce.push(b);
-    ce.map(function(i){i.unshift(0); i.push(0);});
+    ce.map(function(i) {
+      i.unshift(0); i.push(0);
+    });
   }
 
   function shrinkUniverse() {
-    ce = ce.map(function(e){ return e.slice(minX, maxX + 1);});
+    ce = ce.map(function(e) { 
+      return e.slice(minX, maxX + 1);
+    });
     ce = ce.slice(minY, maxY + 1);
   }
 
